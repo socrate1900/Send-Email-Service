f7fdaa3e9a9e4589b43d9d199dc50883726273b4
lavs2 <lavs2@Lavanya.fios-router.home>
2020-03-13 13:15:17 -0400

    Send Email Service

---
+++ VendorManagement Send Email Service/src/main/java/com/sendemail/VendorManagement22/EmailService.java
@@ -0,0 +1,5 @@
+package com.sendemail.VendorManagement22;
+
+public interface EmailService {
+
+}
---
+++ VendorManagement Send Email Service/src/main/java/com/sendemail/VendorManagement22/EmailServiceImpl.java
@@ -0,0 +1,45 @@
+package com.sendemail.VendorManagement22;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailServiceImpl implements EmailService {
+
+	@Autowired
+	public JavaMailSender emailSender;
+
+	public void sendSimpleMessage(String to, String subject, String text) {
+
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(text);
+		emailSender.send(message);
+
+	}
+
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+
+		mailSender.setUsername("my.gmail@gmail.com");
+		mailSender.setPassword("password");
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
+	}
+}
